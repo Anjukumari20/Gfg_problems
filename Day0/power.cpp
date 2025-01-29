00633e0fe7d3e57b79759115392a3a69cbb02537
anjuk <anjukumari0720@gmail.com>
2025-01-29 21:17:32 +0530

    day0

---
+++ Day0/power.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+//Implement the function power(b, e), which calculates b raised to the power of e (i.e. be).
+class Solution {
+  public:
+    double power(double b, int e) {
+        // code here
+        return pow(b,e);
+    }
+};
